d22434bf6ef113872c3d4f41c9034581a8b46900
Khusaeni <43690001+microclonedevcoder@users.noreply.github.com>
2020-02-05 00:41:10 +0700

    IPTfi.js
    Internet Protocol Tracking full information

---
+++ IPTrackerFI.py
@@ -0,0 +1,77 @@
+###################################################################
+## Program IP Tracker Full Information by System Server Security ##
+## author : Khusaeni                                             ##
+## team : System Server Security                                 ##
+## selengkapnya : https://www.systemserversecurity.org           ##
+###################################################################
+
+from __future__ import print_function
+
+import os
+import time
+import json
+import random
+#############################################################################################
+# IP Tracker full information
+# Target =
+# Negara :
+# Provinsi :
+# Kabupaten/Kota :
+# Kecamatan :
+# Desa :
+# Nama Jalan :
+# RT & RW :
+# Arah Jalanya kemana? :
+# Orangnya seperti apa? :
+#
+# merekam aktivitas target dengan kamera depan & belakang
+# Screenshot face target using camera belakang dan depan
+# Google Map
+# Notifikasi dan Getar serta arah HIJAU untuk benar dan MERAH artinya salah
+# ipinfo.io
+# Satelit
+#/*
+#IPv4
+#IPv5
+#IPv6
+#*/
+#//Mac Address / 255.255.x.x
+#//Localhost / 127.0.0.1
+#// 192.168.1.1
+##########################################################################################
+def lacak():
+def ciduk():
+def get_target_ip():
+def ip():
+def check():
+def api():
+def ipinfo():
+def google_map():
+def address(target,map):
+def macaddress():
+def localhost():
+def information():
+    print('''
+    8.8.8.8 / DNS
+    Port :
+    Host :
+    RT :
+    RW :
+    Jalan :
+    Desa :
+    Kecamatan :
+    Kabupaten :
+    Provinsi :
+    Negara :
+    Benua :
+    Planet :
+    Jarak :
+    Notifikasi jalan kemana lagi? :
+    #... Bujur : 142 LS 186 LB
+    ''')
+def menu():
+    print('''
+    [ IP TRACKER FULL INFORMATION ]
+
+    masukan target IP :
+    ''')
