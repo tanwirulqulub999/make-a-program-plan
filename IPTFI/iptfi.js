76bb8c7a619a96624007728d61f94f65dac1a769
Khusaeni <43690001+qolbinnadhzifi@users.noreply.github.com>
2020-02-07 19:26:49 +0700

    ip tracking full information
    this tool for tracking your location

---
+++ IPTFI/iptfi.js
@@ -0,0 +1,8 @@
+/*
+IP/GPS tracking full information by Khusaeni Al-Qolbinnadhzifi
+*/
+
+const ip = require('ip');
+const maps = require('google-maps');
+const mac = require('mac');
+const host = require('localhost');
